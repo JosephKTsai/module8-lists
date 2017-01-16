3f62acf6b6ce1e7aa6783e760bb30862d0bbb547
Joseph Tsai <josephkt@uw.edu>
2017-01-16 15:11:28 -0800

    Fix mistakes in exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,36 +1,30 @@
 # Exercise 1: Lists
 
 # Create a vector of everything you ate for breakfast
-
-breakfast <- c("eggs", "toast", "bacon")
+my.breakfast <- c("eggs", "toast", "bacon")
 
 # Create a vector of everything you ate for lunch
+my.lunch <- c("sandwich", "chips", "cookies")
 
-lunch <- c("sandwich", "chips", "cookies")
 # Create a list "meals" that has contains your `breakfast` and `lunch`
-
-meals <- list (breakfast = breakfast, lunch = lunch)
+meals <- list (breakfast = my.breakfast, lunch = my.lunch)
 
 # Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
-
-meals[[3]] <- (dinner = "steak")
+meals$dinner <- c('steak', 'fries')
 
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
 dinner <- meals$dinner
 
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason)
-
-meals[[5]] <- meals$lunch
+meals[[5]] <- meals[['lunch']]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early.meals`
-
-early.meals <- list(morning = meals[breakfast], afternoon = meals[lunch])
+early.meals <- meals[1:2]
 
 ### Bonus ###
 # Create a list that has the number of items you ate for each meal
@@ -40,14 +34,11 @@ early.meals <- list(morning = meals[breakfast], afternoon = meals[lunch])
 how.many.items <- lapply(meals, length)
 
 # Write a function that adds pizza to every meal
-
-
-# Why isn't my code appending "pizza" to every list? 
-# Isn't list.of.meals referencing what's passed in, i.e. meals?
-# Do I have to reutn it and assign it to meals?
+# Creates a new list that adds pizza to the end of each element in the list
 add.pizza.to.every.meal <- function(list.of.meals){
   list.of.meals <- c(list.of.meals, 'pizza')
 }
 
 # Add pizza to every meal!
+# Need to reassign the output to means because it has created new vectors 
 meals <- lapply(meals, add.pizza.to.every.meal)
